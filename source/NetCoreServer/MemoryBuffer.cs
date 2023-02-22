41753d02e9a4d4d3ab38c063eedf9841d9549969
DJGosnell <DJGosnell@gmail.com>
2023-02-22 00:59:17 -0500

    Added pinned buffers.

---
+++ source/NetCoreServer/MemoryBuffer.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Buffers;
+using System.Diagnostics;
+using System.Text;
+
+namespace DtronixIpc;
+
+/// <summary>
+/// Dynamic T buffer
+/// </summary>
+internal class MemoryBuffer<T>
+    where T : struct
+{
+    private readonly MemoryBufferPool<T> _pool;
+
+    private Memory<T> _data;
+
+    private int _size;
+
+    /// <summary>
+    /// Is the buffer empty?
+    /// </summary>
+    public bool IsEmpty => _size == 0;
+
+    /// <summary>
+    /// Bytes memory buffer capacity
+    /// </summary>
+    public int Capacity => _data.Length;
+    /// <summary>
+    /// Bytes memory buffer size
+    /// </summary>
+    public int Size => _size;
+
+    public int Remaining => _data.Length - _size;
+
+    private int _offset = 0;
+
+    public int Offset => _offset;
+
+    public Memory<T> Contents => _data.Slice(_offset, _size - _offset);
+    public Memory<T> Data => _data;
+
+
+    /// <summary>
+    /// Initialize a new expandable buffer with the given data
+    /// </summary>
+    internal MemoryBuffer(MemoryBufferPool<T> pool, in Memory<T> data)
+    {
+        _pool = pool;
+        _data = data;
+        _size = 0;
+    }
+
+    public void AddOffset(int offset)
+    {
+        _offset += offset;
+    }
+
+    // Clear the current buffer and its offset
+    public void Clear()
+    {
+        _size = 0;
+        _offset = 0;
+    }
+
+    /// <summary>
+    /// Append the single T
+    /// </summary>
+    /// <param name="value">Byte value to append</param>
+    /// <returns>Count of append bytes</returns>
+    public int Append(T value)
+    {
+        _data.Span[_size] = value;
+        _size += 1;
+        return 1;
+    }
+
+    /// <summary>
+    /// Append the given span of bytes
+    /// </summary>
+    /// <param name="buffer">Buffer to append as a span of bytes</param>
+    /// <returns>Count of append bytes</returns>
+    public int Append(in ReadOnlySpan<T> buffer)
+    {
+        
+        buffer.CopyTo(_data.Span.Slice(_size));
+        _size += buffer.Length;
+        return buffer.Length;
+    }
+
+    /// <summary>
+    /// Append the given span of bytes
+    /// </summary>
+    /// <param name="buffer">Buffer to append as a span of bytes</param>
+    /// <returns>Count of append bytes</returns>
+    public int Append(in ReadOnlySequence<T> buffer)
+    {
+        var length = (int)buffer.Length;
+
+        buffer.CopyTo(_data.Span.Slice(_size));
+        _size += length;
+        return length;
+    }
+
+    public void ReturnToPool()
+    {
+        _pool.Return(this);
+    }
+}
---
+++ source/NetCoreServer/MemoryBufferPool.cs
@@ -0,0 +1,52 @@
+﻿using NetCoreServer;
+using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DtronixIpc;
+
+internal class MemoryBufferPool<T>
+    where T : struct
+{
+    private readonly int _bufferSize;
+    private readonly int _maxBuffers;
+
+    private readonly ConcurrentBag<MemoryBuffer<T>> _freeBuffers = new ConcurrentBag<MemoryBuffer<T>>();
+
+    private readonly T[] _buffer;
+
+    private int consumedBufferIndex = -1;
+
+    public MemoryBufferPool(int bufferSize, int maxBuffers)
+    {
+        _bufferSize = bufferSize;
+        _maxBuffers = maxBuffers;
+
+        _buffer = GC.AllocateUninitializedArray<T>((bufferSize * maxBuffers), true);
+    }
+
+    private bool TryRent(out MemoryBuffer<T>? buffer)
+    {
+        if (!_freeBuffers.TryTake(out buffer))
+        {
+            var index = Interlocked.Increment(ref consumedBufferIndex);
+            if (index > _maxBuffers)
+                return false;
+
+            buffer = new MemoryBuffer<T>(this, new Memory<T>(_buffer, index * _bufferSize, _bufferSize))
+        }
+
+        return true;
+    }
+
+    public void Return(MemoryBuffer<T> memoryBuffer)
+    {
+        _freeBuffers.Add(memoryBuffer);
+    }
+}
